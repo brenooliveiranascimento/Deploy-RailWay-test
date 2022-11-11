daedcc17da6441a3b4e5925c798a8e1ee6eb7895
Breno Nascimento <satsukebare23@gmail.com>
2022-11-10 22:39:17 -0300

    updates

--- backEnd/src/index.ts
+++ backEnd/src/index.ts
@@ -11,11 +11,11 @@ app.use(cors())
 
 app.use(express.json());
 
-const PORT = process.env.BD_PORT;
+const PORT = process.env.PORT || 3001;
 app.use('/modules', moduleRoutes)
 app.use('/auth', authRoutes);
 app.use(errorMiddleware)
 
 app.listen(PORT, () => {
-  console.log(`Server is running in http://localhost:${PORT}`)
+  console.log(`Server is running in http://localhost:`, PORT)
 })
